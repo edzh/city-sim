eea09f2bdfb379d8047e183f9da1d24e99c7f9f2
Edwin Zhou <edwinzzhou@gmail.com>
2015-10-03 23:37:47 -0400

    Readd player camera and randomly generate city size

--- js/index.js
+++ js/index.js
@@ -3,62 +3,33 @@
 'use strict';
 var world, scene, renderer;
 var camera, light, controls;
-var buildingShape, buildingMat, buildingGeo,
-    ground, groundShape, groundBody, groundMat, groundGeo,
-    player, playerShape, playerBody, playerMatC, playerGeo;
-// var velocity = new CANNON.Vec3();
+var ground, groundShape, groundBody, groundMat, groundGeo,
+    playerShape, playerBody, playerMatC, playerGeo;
+
 const keys = [];
 
-// var buildingProp = [];
-// var buildingBodyArr = [];
 var buildingShapeArr = [];
 
-// var buildingAmountx = 2;
-// var buildingAmounty = 3;
-
-var citySizex = 4;
-var citySizey = 2;
+var citySizex = Math.random()*15 + 5;
+var citySizey = Math.random()*5 + 1;
 
 var cityArray = [];
 
 function initScene() {
+
+  //Create physics world
   world = new CANNON.World();
   world.gravity.set(0, 0, -98);
   world.broadphase = new CANNON.NaiveBroadphase();
   world.solver.iterations = 10;
 
-  //Add objects here
-
-  //Ground
-  {
-    groundShape = new CANNON.Plane();
-    groundBody = new CANNON.Body({
-      mass: 0,
-      shape: groundShape
-    });
-    world.addBody(groundBody);
-  }
-
-  //Player
-  playerShape = new CANNON.Box(new CANNON.Vec3(2, 2, 4));
-  playerBody = new CANNON.Body({
-    mass: 8,
-    shape: playerShape,
-    // fixedRotation: true,
-    material: playerMatC
-  });
-  playerBody.position.set(0, 0, 200);
-  world.addBody(playerBody);
-// }
-
-// function initThree() {
   //Scene elements
   {
     scene = new THREE.Scene();
 
     //Add camera
     camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight);
-    camera.position.set(0, -100, 500);
+    camera.position.set(0, 0, 50);
 
     //Add light sources
     const directLight = new THREE.DirectionalLight(0xffffff, 0.7);
@@ -68,10 +39,20 @@ function initScene() {
     scene.add(light);
   }
 
-  //Ground
+  //Add objects here
+
+  //Ground Physics
   {
+    groundShape = new CANNON.Plane();
+    groundBody = new CANNON.Body({
+      mass: 0,
+      shape: groundShape
+    });
+    world.addBody(groundBody);
+
+  //Ground Render
     groundMat = new THREE.MeshPhongMaterial({
-      color: 0x4C3380,
+      color: 0x3D2966,
       side: THREE.Frontside
     });
     groundGeo = new THREE.PlaneGeometry(10000, 10000);
@@ -80,16 +61,26 @@ function initScene() {
     scene.add(ground);
   }
 
-  //Building
-
-
   //Player
-  playerMatC = new THREE.MeshPhongMaterial({
-    color: 0xFF66cc,
-  });
-  playerGeo = new THREE.BoxGeometry(4, 4, 8);
-  playerShape = new THREE.Mesh(playerGeo, playerMatC);
-  scene.add(playerShape);
+  {
+    playerShape = new CANNON.Box(new CANNON.Vec3(2, 2, 4));
+    playerBody = new CANNON.Body({
+      mass: 8,
+      shape: playerShape,
+      // fixedRotation: true,
+      material: playerMatC
+    });
+    playerBody.position.set(0, 0, 1000);
+    world.addBody(playerBody);
+
+    //Player render
+    playerMatC = new THREE.MeshPhongMaterial({
+      color: 0xFF66cc
+    });
+    playerGeo = new THREE.BoxGeometry(4, 4, 8);
+    playerShape = new THREE.Mesh(playerGeo, playerMatC);
+    scene.add(playerShape);
+  }
 
   //Renderer
   {
@@ -102,42 +93,45 @@ function initScene() {
 
     //Orbital Controls for camera
     controls = new THREE.OrbitControls(camera, renderer.domElement);
+  }
 
-    for (var i = 0; i < citySizex; i++) {
-      cityArray[i] = [];
-      for (var j = 0; j < citySizey; j++) {
-        var buildingsX = 3;
-        var buildingsY = 5;
-        createBuildingBlock(buildingsX, buildingsY, i, j);
-      }
+  for (var i = 0; i < citySizex; i++) {
+    cityArray[i] = [];
+    for (var j = 0; j < citySizey; j++) {
+      var buildingsX = 2;
+      var buildingsY = 2;
+      createBuildingBlock(buildingsX, buildingsY, i, j);
+      console.log(buildingsX + " " + buildingsY);
     }
   }
 
-  function createBuildingBlock(buildingAmountx, buildingAmounty, blockPosX, blockPosY) {
+  function createBuildingBlock(buildingAmountx, buildingAmounty, blockPosX, blockPosY, past_x, past_y) {
     var buildingProp = [];
     var buildingBodyArr = [];
 
     var buildingShape, buildingMat, buildingGeo;
 
+    //Create building physics
     for (var i = 0; i < buildingAmountx; i++) {
       buildingProp[i] = [];
       buildingBodyArr[i] = [];
       for (var j = 0; j < buildingAmounty; j++) {
-        buildingProp[i][j] = new Building(0, 25, 25, Math.random()*75+50);
+        buildingProp[i][j] = new Building(0, 100, 100, Math.random()*400+100);
         buildingShape = new CANNON.Box(new CANNON.Vec3(buildingProp[i][j].x, buildingProp[i][j].y, buildingProp[i][j].z));
         buildingBodyArr[i][j] = new CANNON.Body({
           mass: 0,
           shape: buildingShape,
-          position: new CANNON.Vec3(blockPosX * (buildingAmountx * 50 + 25) + (buildingProp[i][j].x * i * 2), blockPosY * (buildingAmounty * 50 + 25) + (buildingProp[i][j].y * j * 2), buildingProp[i][j].z)
+          position: new CANNON.Vec3(blockPosX * (buildingAmountx * 200 + 100) + (buildingProp[i][j].x * i * 2.1),
+            blockPosY * (buildingAmounty * 200 + 100) + (buildingProp[i][j].y * j * 2.1), buildingProp[i][j].z)
         });
         world.addBody(buildingBodyArr[i][j]);
-
       }
     }
 
+    //Building render
     buildingMat = new THREE.MeshPhongMaterial({
-    color: 0x9966FF,
-    side: THREE.Frontside
+    color: 0x9966FF + Math.random(),
+    // side: THREE.Frontside
     // wireframe: true
     });
 
@@ -177,19 +171,21 @@ function animate() {
   if (keys[65]) {
     playerBody.velocity.x -= speed;
   }
-  if (keys[32] && playerBody.position.z <= 5) {
-    playerBody.velocity.z += 24;
+  if (keys[32]) {
+    playerBody.velocity.z += 4;
   }
+
   // console.log(playerBody.position.z);
   // console.log(velocity.x + " " + velocity.y);
   // playerBody.velocity.mult(0.99, playerBody.velocity);
+  camera.position.set(playerBody.position.x, playerBody.position.y - 25, playerBody.position.z + 5);
+  camera.lookAt(playerBody.position);
   renderer.render(scene, camera);
 
  }
 
 function updatePhys() {
   world.step(1/60);
-
 
   playerShape.position.copy(playerBody.position);
   playerShape.quaternion.copy(playerBody.quaternion);
