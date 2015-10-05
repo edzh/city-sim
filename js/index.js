27eb8c3b638723c0c5c98b215ee8ac4b601b3786
Edwin Zhou <edwinzzhou@gmail.com>
2015-10-05 00:09:06 -0400

    Remove building.js, start sidewalk building

--- js/index.js
+++ js/index.js
@@ -10,10 +10,19 @@ const keys = [];
 
 var buildingShapeArr = [];
 
-var citySizex = Math.random()*15 + 5;
-var citySizey = Math.random()*5 + 1;
+var citySizex = Math.random()*5 + 1;
+var citySizey = Math.random()*15 + 5;
 
 var cityArray = [];
+
+//Building Constructor
+var Building = function(mass, x, y, z, color) {
+  this.mass = mass;
+  this.x = x;
+  this.y = y;
+  this.z = z;
+  this.color = 0xaaaaaa;
+};
 
 function initScene() {
 
@@ -105,45 +114,69 @@ function initScene() {
     }
   }
 
-  function createBuildingBlock(buildingAmountx, buildingAmounty, blockPosX, blockPosY, past_x, past_y) {
-    var buildingProp = [];
+  function createBuildingBlock(buildingAmountx, buildingAmounty, blockPosX, blockPosY) {
+    var buildingHeight = [];
     var buildingBodyArr = [];
 
     var buildingShape, buildingMat, buildingGeo;
+    var sidewalkShape, sidewalkMat, sidewalkGeo, sidewalkBody, sidewalk;
+
+    var buildingSizex = 100;
+    var buildingSizey = 100;
+    var roadSize = 150;
+    var sidewalkSizex = buildingSizex * buildingAmountx;
+    var sidewalkSizey = buildingSizey * buildingAmounty;
 
     //Create building physics
     for (var i = 0; i < buildingAmountx; i++) {
-      buildingProp[i] = [];
+      buildingHeight[i] = [];
       buildingBodyArr[i] = [];
       for (var j = 0; j < buildingAmounty; j++) {
-        buildingProp[i][j] = new Building(0, 100, 100, Math.random()*400+100);
-        buildingShape = new CANNON.Box(new CANNON.Vec3(buildingProp[i][j].x, buildingProp[i][j].y, buildingProp[i][j].z));
+        buildingHeight[i][j] = Math.random()*400+100;
+        buildingShape = new CANNON.Box(new CANNON.Vec3(buildingSizex, buildingSizey, buildingHeight[i][j]));
         buildingBodyArr[i][j] = new CANNON.Body({
           mass: 0,
           shape: buildingShape,
-          position: new CANNON.Vec3(blockPosX * (buildingAmountx * 200 + 100) + (buildingProp[i][j].x * i * 2.1),
-            blockPosY * (buildingAmounty * 200 + 100) + (buildingProp[i][j].y * j * 2.1), buildingProp[i][j].z)
+          position: new CANNON.Vec3(
+            blockPosX * (buildingAmountx * buildingSizex * 2 + roadSize) + (buildingSizex * i * 2),
+            blockPosY * (buildingAmounty * buildingSizey * 2 + roadSize) + (buildingSizey * j * 2),
+            buildingHeight[i][j]
+          )
         });
         world.addBody(buildingBodyArr[i][j]);
       }
     }
 
+    sidewalkShape = new CANNON.Box(new CANNON.Vec3(sidewalkSizex, sidewalkSizey, 1));
+    sidewalkBody = new CANNON.Body({
+      mass: 0,
+      shape: sidewalkShape,
+      position: new CANNON.Vec3(buildingAmountx * buildingSizex, buildingAmountx * buildingSizey, 1)
+    });
+    sidewalkMat = new THREE.MeshPhongMaterial({color: 0xFFFFFFF});
+    sidewalkGeo = new THREE.BoxGeometry(sidewalkSizex * 2.25, sidewalkSizey * 2.25, 2);
+    sidewalk = new THREE.Mesh(sidewalkGeo, sidewalkMat);
+    scene.add(sidewalk);
+    sidewalk.position.copy(sidewalkBody.position);
+    sidewalk.quaternion.copy(sidewalkBody.quaternion);
+
     //Building render
     buildingMat = new THREE.MeshPhongMaterial({
-    color: 0x9966FF + Math.random(),
-    // side: THREE.Frontside
-    // wireframe: true
+      color: 0x9966FF
+      // side: THREE.Frontside
+      // wireframe: true
     });
 
     for (i = 0; i < buildingAmountx; i++) {
       buildingShapeArr[i] = [];
       for (j = 0; j < buildingAmounty; j++) {
-        buildingGeo = new THREE.BoxGeometry(buildingProp[i][j].x * 2, buildingProp[i][j].y * 2, buildingProp[i][j].z * 2);
+        buildingGeo = new THREE.BoxGeometry(buildingSizex * 2, buildingSizey * 2, buildingHeight[i][j] * 2);
         buildingShapeArr[i][j] = new THREE.Mesh(buildingGeo, buildingMat);
         scene.add(buildingShapeArr[i][j]);
       }
     }
-      for ( i = 0; i < buildingAmountx; i++) {
+
+    for ( i = 0; i < buildingAmountx; i++) {
       for ( j = 0; j < buildingAmounty; j++) {
         buildingShapeArr[i][j].position.copy(buildingBodyArr[i][j].position);
         buildingShapeArr[i][j].quaternion.copy(buildingBodyArr[i][j].quaternion);
@@ -153,6 +186,7 @@ function initScene() {
     // console.log(world.bodies);
     // console.log(scene.children);
 }
+
 
 function animate() {
   requestAnimationFrame(animate);
@@ -178,7 +212,7 @@ function animate() {
   // console.log(playerBody.position.z);
   // console.log(velocity.x + " " + velocity.y);
   // playerBody.velocity.mult(0.99, playerBody.velocity);
-  camera.position.set(playerBody.position.x, playerBody.position.y - 25, playerBody.position.z + 5);
+  camera.position.set(playerBody.position.x, playerBody.position.y, playerBody.position.z + 100);
   camera.lookAt(playerBody.position);
   renderer.render(scene, camera);
 
